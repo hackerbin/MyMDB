58414f03d4a7040a2d0ddf55907752923759c81f
Robin <robin@priyo.com>
2018-09-12 16:39:23 +0600

    adding list and details view and template

--- core/views.py
+++ core/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.views.generic import (ListView, DetailView)
+from core.models import Movie
 
-# Create your views here.
+
+class MovieDetail(DetailView):
+    model = Movie
+
+
+class MovieList(ListView):
+    model = Movie
+
